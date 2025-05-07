db0cd6e94b38d531251c7a3de75264e0e8612781
jg24aat@herts.ac.uk <jg24aat@herts.ac.uk>
2025-05-07 23:47:55 +0100

    created agent feature

---
+++ agent.py
@@ -0,0 +1,35 @@
+from langchain.schema import HumanMessage, SystemMessage
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+openai_key = os.getenv("OPENAI_APIKEY")
+
+llm_name ="gpt-3.5-turbo"
+model = ChatOpenAI(api_key=openai_key, model=llm_name)
+
+messages = [
+    SystemMessage(content="You are a helpful assistant who is extremely competent as a computer Scientist! Your name is Rob."),
+    HumanMessage(content="who was the very first computer scientist"),
+]
+
+# res = model.invoke(messages)
+# print(res)
+
+def first_agent(messages):
+    res = model.invoke(messages)
+    return res
+def run_agent():
+    print("simple AI Agent: Type 'exit' to quit")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower()== "exit":
+            print("Goodbye")
+            break
+        print("AI Agent is thinking......")
+        messages = [HumanMessage(content=user_input)]
+        response = first_agent(messages)
+        print("AI Agent: getting the response........")
+        print(f"AI Agent: {response.content}")
